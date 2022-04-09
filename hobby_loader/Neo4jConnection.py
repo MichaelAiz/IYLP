2110358fcd596abacfe3d7829b1848c0b2ac4d0e
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2022-04-09 13:43:21 -0400

    some changes to hobby laoder and node setup

--- hobby_loader/Neo4jConnection.py
+++ hobby_loader/Neo4jConnection.py
@@ -8,19 +8,22 @@ class Neo4jConnections:
     def close(self):
         self.driver.close()
     
-    def _add_hobby(self, tx, hobby_name, hobby_id):
+    def transfunc_add_hobby(self, tx, hobby_name, hobby_id):
             query = (
                 "CREATE (h:Hobby { name: $hobby_name, id: $hobby_id }) "
                 "RETURN h"
             )
-            tx.run(query, hobby_name = hobby_name, hobby_id = hobby_id)  
+            try:
+                tx.run(query, hobby_name = hobby_name, hobby_id = hobby_id)  
+            except:
+                raise
     def add_hobby(self, hobby_name, hobby_id):
         with self.driver.session() as session:
             result = session.write_transaction(
-                self._add_hobby, hobby_name, hobby_id
+                self.transfunc_add_hobby, hobby_name, hobby_id
             )
 
-    def _delete_all_hobbies(self, tx):
+    def transfunc_delete_all_hobbies(self, tx):
         query = (
             "MATCH (n)"
             "DETACH DELETE n"
@@ -29,17 +32,17 @@ class Neo4jConnections:
     def delete_all_hobbies(self):
         with self.driver.session() as session:
             session.write_transaction(
-                self._delete_all_hobbies
+                self.transfunc_delete_all_hobbies
             )
 
-    def _add_constraints(self, tx):
+    def transfunc_add_constraints(self, tx):
         query = (
             "CREATE CONSTRAINT unique_hobby_name FOR (hobby:Hobby) REQUIRE hobby.name IS UNIQUE" 
-            "CREATE CONSTRAINT unique_hobby_ID FOR (hobby:Hobby) REQUIRE hobby.id IS UNIQUE" 
         )
-    def create_constraints(self):
+        tx.run(query)
+    def add_constraints(self):
         with self.driver.session() as session:
             session.write_transaction(
-                self._add_constraints
+                self.transfunc_add_constraints
             )
 

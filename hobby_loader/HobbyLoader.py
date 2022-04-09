29096afe5ce722d5a6ff9a7c167472c5ec2c854a
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2022-04-09 12:51:25 -0400

    created constraints method and delete hobbies method

--- hobby_loader/HobbyLoader.py
+++ hobby_loader/HobbyLoader.py
@@ -1,12 +1,15 @@
+from uuid import uuid4
 import requests
 from bs4 import BeautifulSoup
 from neo4j import GraphDatabase
 from Neo4jConnection import Neo4jConnections
 import os
 from dotenv import load_dotenv
+from uuid import uuid4
 
 load_dotenv()
 
+# Gets all the hobbies from wikipedia list of hobbies and adds them to neo4j db
 if __name__ == "__main__":
     uri = os.getenv('DB_URI')
     user = os.getenv('DB_USER')
@@ -20,14 +23,12 @@ if __name__ == "__main__":
     soup = BeautifulSoup(page.content, "html.parser")
 
     groups = soup.find_all(class_="div-col")
-    x = 2
     for group in groups:
         hobbies = group.find_all('a', title = True)
         for hobby in hobbies:
-            while x > 1:
-                print("adding " + hobby.getText())
-                conn.add_hobby(hobby.getText())
-                x -= 1
+            hobby_id = uuid4()
+            print("adding " + hobby.getText())
+            conn.add_hobby(hobby.getText(), hobby_id)
 
     conn.close()
 
--- hobby_loader/Neo4jConnection.py
+++ hobby_loader/Neo4jConnection.py
@@ -8,24 +8,38 @@ class Neo4jConnections:
     def close(self):
         self.driver.close()
     
-    def _add_and_return_hobby(self, tx, hobby_name):
+    def _add_hobby(self, tx, hobby_name, hobby_id):
             query = (
-                "CREATE (h:Hobby { name: $hobby_name }) "
+                "CREATE (h:Hobby { name: $hobby_name, id: $hobby_id }) "
                 "RETURN h"
             )
-            result = tx.run(query, hobby_name = hobby_name)
-            for row in result:
-                print(row['h'])
-            return result
-
-        
-    def add_hobby(self, hobby_name):
+            tx.run(query, hobby_name = hobby_name, hobby_id = hobby_id)  
+    def add_hobby(self, hobby_name, hobby_id):
         with self.driver.session() as session:
             result = session.write_transaction(
-                self._add_and_return_hobby, hobby_name
+                self._add_hobby, hobby_name, hobby_id
             )
-    
-if __name__ == "__main__":
-    print('hello')
-    conn = Neo4jConnections("yeah", "2", "3")
+
+    def _delete_all_hobbies(self, tx):
+        query = (
+            "MATCH (n)"
+            "DETACH DELETE n"
+        )
+        tx.run(query)
+    def delete_all_hobbies(self):
+        with self.driver.session() as session:
+            session.write_transaction(
+                self._delete_all_hobbies
+            )
+
+    def _add_constraints(self, tx):
+        query = (
+            "CREATE CONSTRAINT unique_hobby_name FOR (hobby:Hobby) REQUIRE hobby.name IS UNIQUE" 
+            "CREATE CONSTRAINT unique_hobby_ID FOR (hobby:Hobby) REQUIRE hobby.id IS UNIQUE" 
+        )
+    def create_constraints(self):
+        with self.driver.session() as session:
+            session.write_transaction(
+                self._add_constraints
+            )
 

1d1bc48c771431e728eab2c95da2d45865a731ea
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2022-04-10 15:56:02 -0400

    new routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,19 @@
 import Fastify from 'fastify';
-import login from './login.js'
+import login from './api/auth/login.js'
 
 const fastify = Fastify();
 
-fastify.register(login)
+fastify.route(login)
 
-console.log("doing stuff")
+// Declare a route
+// fastify.get('/', function (request, reply) {
+//     reply.send({ hello: 'world' })
+//   })
 
 const start = async () => {
     try {
         console.log("starting")
-        await fastify.listen(3001)
+        await fastify.listen(3000, "0.0.0.0")
     } catch (err) {
         fastify.log.error(err)
     }

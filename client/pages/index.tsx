8368dbdce59afd4a41e050d34ebf71a525fd6521
Michael Aizenshtadt <michael.aizenshtadt@gmail.com>
2022-04-13 00:27:52 -0400

    basic next app setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <div>Hello World</div>
+    )
+}
+    
+export default Home;
